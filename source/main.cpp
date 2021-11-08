55caa03b8ef29479ca0c7fd3e217ef11aa1a4eec
flisoch <flisochcs@gmail.com>
2021-11-09 02:23:34 +0300

    sending sensors data message, not 'helloworld'

--- source/main.cpp
+++ source/main.cpp
@@ -14,88 +14,92 @@
  *    Ian Craggs - initial API and implementation and/or initial documentation
  *    Ian Craggs - make sure QoS2 processing works, and add device headers
  *******************************************************************************/
- 
- /**
-  This is a sample program to illustrate the use of the MQTT Client library
-  on the mbed platform.  The Client class requires two classes which mediate
-  access to system interfaces for networking and timing.  As long as these two
-  classes provide the required public programming interfaces, it does not matter
-  what facilities they use underneath. In this program, they use the mbed
-  system libraries.
- 
- */
- 
+
+/**
+ This is a sample program to illustrate the use of the MQTT Client library
+ on the mbed platform.  The Client class requires two classes which mediate
+ access to system interfaces for networking and timing.  As long as these two
+ classes provide the required public programming interfaces, it does not matter
+ what facilities they use underneath. In this program, they use the mbed
+ system libraries.
+
+*/
 
 
-#define logMessage printf
- 
 #include "MQTTmbed.h"
 #include <MQTTClientMbedOs.h>
- 
- 
-int main(int argc, char* argv[])
-{
-    logMessage("Starting HelloMQTT demo:\n");
- 
-    NetworkInterface *net = NetworkInterface::get_default_instance();
-    if (!net) {
-        printf("Error! No network inteface found.\n");
-        return 0;
-    }
-    logMessage("Connecting to the network...\r\n");
+#include <cstdio>
 
-    /* connect will perform the action to connect to the network */
-    nsapi_size_or_error_t rc = net->connect();
-    if (rc != 0) {
-        printf("Error! _net->connect() returned: %d\r\n", rc);
-        return -1;
-    }
+int main(int argc, char *argv[]) {
+  printf("Starting HelloMQTT demo:\n");
 
-    /* opening the socket only allocates resources */
-    TCPSocket socket;
-    rc = socket.open(net);
-    if (rc != 0) {
-        printf("Error! _socket.open() returned: %d\r\n", rc);
-        return -1;
-    }
-
-    /* now we have to find where to connect */
-    SocketAddress address;
-    net->gethostbyname("utrlr3.messaging.internetofthings.ibmcloud.com", &address);
-    address.set_port(1883);
-
-    /* we are connected to the network but since we're using a connection oriented
-    * protocol we still need to open a connection on the socket */
-    logMessage("Opening connection to remote %s port %d\r\n", address.get_ip_address(), address.get_port());
-    rc = socket.connect(address);
-    if (rc != 0) {
-        printf("Error! _socket.connect() returned: %d\r\n", rc);
-        return -1;
-    }
-    logMessage("Connected socket\n");
-
-    /* MQTT connection */
-    MQTTClient client(&socket);
-    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
-    data.MQTTVersion = 3;
-    data.clientID.cstring = "d:orgid:STM32Board:my_test_board";
-    data.username.cstring = "use-token-auth";
-    data.password.cstring = "change-me";
-    if ((rc = client.connect(data)) != 0)
-        logMessage("rc from MQTT connect is %d\r\n", rc);
-
-    MQTT::Message message;
-    char* topic = "iot-2/evt/statusEvent/fmt/json"; 
-    char buf[100];
-    sprintf(buf, "{\"d\": \"Hello World!\"}");
-    message.qos = MQTT::QOS0;
-    message.retained = false;
-    message.dup = false;
-    message.payload = (void*)buf;
-    message.payloadlen = strlen(buf);
-    rc = client.publish(topic, message);
-    logMessage("Message sent\n");
-    logMessage("Demo concluded successfully \r\n");
-
+  TCPSocket socket;
+  NetworkInterface *net = NetworkInterface::get_default_instance();
+  if (!net) {
+    printf("Error! No network inteface found.\n");
     return 0;
+  }
+  printf("Connecting to the network...\r\n");
+
+  /* connect will perform the action to connect to the network */
+  nsapi_size_or_error_t rc = net->connect();
+  if (rc != 0) {
+    printf("Error! _net->connect() returned: %d\r\n", rc);
+    return -1;
+  }
+
+  /* opening the socket only allocates resources */
+  rc = socket.open(net);
+  if (rc != 0) {
+    printf("Error! _socket.open() returned: %d\r\n", rc);
+    return -1;
+  }
+
+  /* now we have to find where to connect */
+  SocketAddress address;
+  net->gethostbyname("utrlr3.messaging.internetofthings.ibmcloud.com",
+                     &address);
+  address.set_port(1883);
+
+  /* we are connected to the network but since we're using a connection oriented
+   * protocol we still need to open a connection on the socket */
+  printf("Opening connection to remote %s port %d\r\n",
+         address.get_ip_address(), address.get_port());
+  rc = socket.connect(address);
+  if (rc != 0) {
+    printf("Error! _socket.connect() returned: %d\r\n", rc);
+    return -1;
+  }
+  printf("Connected socket\n");
+
+  /* MQTT connection */
+  MQTTClient client(&socket);
+  MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
+  data.MQTTVersion = 3;
+  data.clientID.cstring = "d:utrlr3:STM32Board:my_test_board";
+  data.username.cstring = "use-token-auth";
+  data.password.cstring = "c)-rMerJ@9Ix0Kk?0@";
+  if ((rc = client.connect(data)) != 0)
+    printf("rc from MQTT connect is %d\r\n", rc);
+
+  MQTT::Message message;
+  char *topic = "iot-2/evt/statusEvent/fmt/json";
+  char buf[100];
+  int temp = 1;
+  int press = 2;
+  int hum = 3;
+  sprintf(buf,
+          "{\"d\":{\"ST\":\"Nucleo-IoT-mbed\",\"Temp\":%d,\"Pressure\":"
+          "%d,\"Humidity\":%d}}", temp, press, hum);
+  printf("Sending message: \n%s\n", buf);
+  message.qos = MQTT::QOS0;
+  message.retained = false;
+  message.dup = false;
+  message.payload = (void *)buf;
+  message.payloadlen = strlen(buf);
+  rc = client.publish(topic, message);
+  printf("Message sent\n");
+  printf("Demo concluded successfully \r\n");
+
+  return 0;
 }
