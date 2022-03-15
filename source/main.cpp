fd1d18e1e358f019792eb9d07a83485e0bbebf26
Eugene A. Kanashev <kanashevea@susu.ru>
2022-03-15 17:35:19 +0500

    periodical publish a message from bme280

--- source/main.cpp
+++ source/main.cpp
@@ -1,101 +1,126 @@
-/*******************************************************************************
- * Copyright (c) 2014, 2015 IBM Corp.
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * and Eclipse Distribution License v1.0 which accompany this distribution.
- *
- * The Eclipse Public License is available at
- *    http://www.eclipse.org/legal/epl-v10.html
- * and the Eclipse Distribution License is available at
- *   http://www.eclipse.org/org/documents/edl-v10.php.
- *
- * Contributors:
- *    Ian Craggs - initial API and implementation and/or initial documentation
- *    Ian Craggs - make sure QoS2 processing works, and add device headers
- *******************************************************************************/
-
-/**
- This is a sample program to illustrate the use of the MQTT Client library
- on the mbed platform.  The Client class requires two classes which mediate
- access to system interfaces for networking and timing.  As long as these two
- classes provide the required public programming interfaces, it does not matter
- what facilities they use underneath. In this program, they use the mbed
- system libraries.
-
-*/
-
-
 #include "MQTTmbed.h"
 #include <MQTTClientMbedOs.h>
 #include <cstdio>
+#include "BME280.h"
 
-int main(int argc, char *argv[]) {
-  printf("Starting ThingsBoard MQTT demo:\n");
+#define MQTT_BROKER "thingsboard.myitacademy.ru"
+#define MQTT_TOPIC  "v1/devices/me/telemetry"
+#define SIG_NEED_PUBLISH    0x01
 
-  TCPSocket socket;
-  NetworkInterface *net = NetworkInterface::get_default_instance();
-  if (!net) {
-    printf("Error! No network inteface found.\n");
+DigitalOut led(LED1);
+BME280 sensor_bme(I2C_SDA, I2C_SCL);
+
+Thread mqtt_thread;
+LowPowerTicker lpTicker;
+Timeout led_timeout;
+
+void led_off_cb(void)
+{
+    led = false;
+}
+
+void tickerIRQ (void)
+{
+    led = true;
+    led_timeout.attach(callback(led_off_cb), 200ms);
+    mqtt_thread.flags_set(SIG_NEED_PUBLISH);
+}
+
+void mqtt_thread_io()
+{
+    nsapi_size_or_error_t rc;
+    printf("Starting ThingsBoard MQTT demo:\n");
+
+    NetworkInterface *net = NetworkInterface::get_default_instance();
+    if (!net)
+    {
+        printf("Error! No network inteface found.\n");
+    }
+    else
+    {
+        do
+        {
+            printf("Connecting to the network...\r\n");
+            /* connect will perform the action to connect to the network */
+            rc = net->connect();
+            if (rc != 0)
+            {
+                printf("Error! _net->connect() returned: %d\r\n", rc);
+                return;
+            }
+
+            TCPSocket socket;
+            /* opening the socket only allocates resources */
+            rc = socket.open(net);
+            if (rc != 0)
+            {
+                printf("Error! _socket.open() returned: %d\r\n", rc);
+                return;
+            }
+
+            /* now we have to find where to connect */
+            SocketAddress address;
+            net->gethostbyname(MQTT_BROKER, &address);
+            address.set_port(1883);
+
+            /* we are connected to the network but since we're using a connection oriented
+            * protocol we still need to open a connection on the socket */
+            printf("Opening connection to remote %s port %d\r\n",
+                address.get_ip_address(), address.get_port());
+            rc = socket.connect(address);
+            if (rc != 0)
+            {
+                printf("Error! _socket.connect() returned: %d\r\n", rc);
+                return;
+            }
+            printf("Connected socket\n");
+
+            /* MQTT connection */
+            MQTTClient client(&socket);
+            MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
+            data.MQTTVersion = 3;
+            data.clientID.cstring = (char*)"MyClientID";
+            data.username.cstring = (char*)"A2_TEST_TOKEN";
+
+            if ((rc = client.connect(data)) != 0)
+                printf("rc from MQTT connect is %d\r\n", rc);
+
+            MQTT::Message message;
+            char buf[100];
+            int temp = (int)sensor_bme.getTemperature();
+            sprintf(buf, "{\"temperature\": %d}", temp);
+            printf("Sending message: \n%s\n", buf);
+            message.qos = MQTT::QOS1;
+            message.retained = false;
+            message.dup = false;
+            message.payload = (void *)buf;
+            message.payloadlen = strlen(buf);
+            rc = client.publish((const char*)MQTT_TOPIC, message);
+            printf("Message sent, rc: %d\n", rc);
+
+            while (true) {
+                ThisThread::flags_wait_all(SIG_NEED_PUBLISH);
+                temp = (int)sensor_bme.getTemperature();
+                sprintf(buf, "{\"temperature\": %d}", temp);
+                message.payload = (void *)buf;
+                message.payloadlen = strlen(buf);
+
+                rc = client.publish((const char*)MQTT_TOPIC, message);
+                if (rc == 0)
+                    printf("Message sent. Temperature = %d\n", temp);
+                else
+                    break;
+            }
+        } while (rc != 0);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    mqtt_thread.start(callback(mqtt_thread_io));
+    lpTicker.attach(tickerIRQ, 30s); // every 30 seconds
+    while(1)
+    {
+    }
     return 0;
-  }
-  printf("Connecting to the network...\r\n");
-
-  /* connect will perform the action to connect to the network */
-  nsapi_size_or_error_t rc = net->connect();
-  if (rc != 0) {
-    printf("Error! _net->connect() returned: %d\r\n", rc);
-    return -1;
-  }
-
-  /* opening the socket only allocates resources */
-  rc = socket.open(net);
-  if (rc != 0) {
-    printf("Error! _socket.open() returned: %d\r\n", rc);
-    return -1;
-  }
-
-  /* now we have to find where to connect */
-  SocketAddress address;
-  net->gethostbyname("thingsboard.myitacademy.ru", &address);
-  address.set_port(1883);
-
-  /* we are connected to the network but since we're using a connection oriented
-   * protocol we still need to open a connection on the socket */
-  printf("Opening connection to remote %s port %d\r\n",
-         address.get_ip_address(), address.get_port());
-  rc = socket.connect(address);
-  if (rc != 0) {
-    printf("Error! _socket.connect() returned: %d\r\n", rc);
-    return -1;
-  }
-  printf("Connected socket\n");
-
-  /* MQTT connection */
-  MQTTClient client(&socket);
-  MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
-  data.MQTTVersion = 3;
-  data.clientID.cstring = "MyClientID";
-  data.username.cstring = "A2_TEST_TOKEN";
-
-  if ((rc = client.connect(data)) != 0)
-    printf("rc from MQTT connect is %d\r\n", rc);
-  
-  MQTT::Message message;
-  char *topic = "v1/devices/me/telemetry";
-  char buf[100];
-  int temp = 25;
-  sprintf(buf,
-          "{\"temperature\":%d}", temp);
-  printf("Sending message: \n%s\n", buf);
-  message.qos = MQTT::QOS0;
-  message.retained = false;
-  message.dup = false;
-  message.payload = (void *)buf;
-  message.payloadlen = strlen(buf);
-  rc = client.publish(topic, message);
-  printf("Message sent, rc: %d\n", rc);
-  printf("Demo concluded successfully \r\n");
-
-  return 0;
 }
